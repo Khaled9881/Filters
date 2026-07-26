9d3ec2adc83de507a9dd4603c02482660c4bacfa
khaled Ibrahiem <khaled.ibrahiem@msa.edu.eg>
2026-07-26 19:14:49 +0300

    Add project files.

---
+++ Filters/Filters/ActionFilters/WeatherActiinFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Filters.Filters.ActionFilters
+{
+    public class WeatherActiinFilter(ILogger<WeatherActiinFilter> logger) : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            logger.LogInformation("After Action ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️");
+
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            logger.LogInformation("Before Action ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️");
+        }
+    }
+}
